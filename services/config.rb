379d8cb52ee14b6452e6dfba22ce214e93635e8d
Paul Allen <pauldallen@gmail.com>
2017-06-05 17:02:22 -0700

    updating methods

--- services/config.rb
+++ services/config.rb
@@ -82,7 +82,8 @@
 #   - list_hosted_zones <- SKIPPING due to @engine_bug_exclusions
 #   - list_resource_record_sets({})
 #   - list_reusable_delegation_sets <- SKIPPING due to @engine_bug_exclusions
-#   - list_traffic_policies <- SKIPPING due to @engine_bug_exclusions
+#   - list_traffic_policies({})
+#     - id: traffic_policy_summaries.id
 #   - list_traffic_policy_instances({})
 #     - id: traffic_policy_instances.id
 #   - list_traffic_policy_versions({})
@@ -108,6 +109,23 @@
 # WorkDocs
 # WorkSpaces
 # XRay
+coreo_aws_rule "route53-inventory-traffic-policies" do
+  service :Route53
+  action :define
+  link "http://kb.cloudcoreo.com/mydoc_all-inventory.html"
+  include_violations_in_count false
+  display_name "Route53 Traffic Policies Inventory"
+  description "This rule performs an inventory on the Route53 service using the list_traffic_policies function"
+  category "Inventory"
+  suggested_action "None."
+  level "Informational"
+  objectives ["list_traffic_policies"]
+  audit_objects ["object.traffic_policy_summaries.id"]
+  operators ["=~"]
+  raise_when [//]
+  id_map ["object.traffic_policy_summaries.id"]
+  
+end
 coreo_aws_rule "route53-inventory-traffic-policy-instances" do
   service :Route53
   action :define
@@ -129,6 +147,6 @@ end
 coreo_aws_rule_runner "route53-inventory-runner" do
   action :run
   service :Route53
-  rules ["route53-inventory-traffic-policy-instances"]
+  rules ["route53-inventory-traffic-policies", "route53-inventory-traffic-policy-instances"]
   regions ['us-east-1']
 end
